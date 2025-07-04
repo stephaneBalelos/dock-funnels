8850804c02c983bc422c48c70addab2bbb2a3ca8
stephaneBalelos <stephanedondyas2@gmail.com>
2025-07-04 12:07:59 +0200

    Styling fixes

--- front-end/src/components/forms/FormFields/FormFieldCheckboxList.vue
+++ front-end/src/components/forms/FormFields/FormFieldCheckboxList.vue
@@ -96,7 +96,7 @@ watch(
 
 const classSettings = computed(() => {
   let base = `flex flex-col py-4`;
-  let label = `text-surface-900 text-lg leading-none mb-3`;
+  let label = `text-surface-900 text-lg font-semibold leading-none mb-3`;
   let optionsContainer = `w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4`;
   if(settings.align) {
     base += ` text-${settings.align}`;
