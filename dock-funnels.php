4a1aae8954edf84b3d61aaefba50bfb29e71b243
stephaneBalelos <stephanedondyas2@gmail.com>
2025-07-15 13:25:10 +0200

    Patch

--- dock-funnels.php
+++ dock-funnels.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Dock Funnels
 Description: Dock Funnels ist ein Minimalistisches aber leistungsstarkes Plugin, das es dir ermöglicht, mehrstufige Formulare zu erstellen und zu verwalten. Es ist ideal für die Erstellung von Umfragen, Anmeldeformularen und anderen Formularen, die mehrere Schritte erfordern.
-Version: 0.9.1
+Version: 0.9.2
 Author: Dock26 
 */
 
