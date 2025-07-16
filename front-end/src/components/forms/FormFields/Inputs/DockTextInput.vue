1417d027dd6e19f58a67532892d44df190d6f685
stephaneBalelos <stephanedondyas2@gmail.com>
2025-07-16 11:01:09 +0200

    dsada

---
+++ front-end/src/components/forms/FormFields/Inputs/DockTextInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="">
+        <input 
+            :type="props.type"
+            :placeholder="props.placeholder"
+            v-model="model"
+            class="dock-text-input" />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+type Props = {
+    type: "text" | "number" | "email" | "tel" | "date";
+    placeholder?: string;
+}
+
+const props = defineProps<Props>();
+
+const model = defineModel<string | null>({
+    type: String,
+    default: null
+});
+
+</script>
+
+<style scoped>
+
+</style>
